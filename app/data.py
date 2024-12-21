ddc12ab6663b766ce8c87626a50872428939538d
mbroton <michal.broton.ns@gmail.com>
2024-12-21 01:05:06 +0100

    Add database functions for workers

--- app/data.py
+++ app/data.py
@@ -1,9 +1,10 @@
-from typing import TypeAlias
+from typing import TypeAlias, Literal
 from datetime import datetime
-from enum import Enum
 
 import aiosqlite
 from pydantic import BaseModel
+
+from app import jobs
 
 AsyncConnection: TypeAlias = aiosqlite.Connection
 
@@ -13,8 +14,8 @@ _INIT_SQL = """
 CREATE TABLE IF NOT EXISTS jobs (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     type TEXT NOT NULL,
-    status TEXT NOT NULL CHECK (status IN ('pending', 'done', 'in_progress', 'failed')),
-    source_type TEXT NOT NULL CHECK (source_type IN ('url', 'html')),
+    status TEXT NOT NULL,
+    source_type TEXT NOT NULL,
     source TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP
@@ -31,34 +32,21 @@ CREATE INDEX IF NOT EXISTS idx_outputs_job_id ON outputs(job_id);
 """
 
 
-class JobType(str, Enum):
-    SCREENSHOT = "screenshot"
-    PDF = "pdf"
-
-
-class JobStatus(str, Enum):
-    PENDING = "pending"
-    DONE = "done"
-    IN_PROGRESS = "in_progress"
-    FAILED = "failed"
-
-
-class JobSourceType(str, Enum):
-    URL = "url"
-    HTML = "html"
-
-
 class DBJob(BaseModel):
     """Represents a job record from the database.
 
     Note: The 'source' field is intentionally excluded from this model."""
 
     id: int
-    type: JobType
-    status: JobStatus
-    source_type: JobSourceType
+    type: jobs.JobType
+    status: jobs.JobStatus
+    source_type: jobs.JobSourceType
     created_at: datetime
     updated_at: datetime | None
+
+
+class DBJobFull(DBJob):
+    source: str
 
 
 class DBOutput(BaseModel):
@@ -83,8 +71,8 @@ async def init_db(conn: AsyncConnection) -> None:
 
 async def create_job(
     conn: AsyncConnection,
-    type_: JobType,
-    source_type: JobSourceType,
+    type_: jobs.JobType,
+    source_type: jobs.JobSourceType,
     source: str,
 ) -> DBJob:
     async with conn.execute(
@@ -93,14 +81,17 @@ async def create_job(
         VALUES (?, ?, ?, ?)
         RETURNING id, created_at, updated_at
         """,
-        (type_, JobStatus.PENDING, source_type, source),
+        (type_, jobs.JobStatus.PENDING, source_type, source),
     ) as cursor:
         result = await cursor.fetchone()
 
     await conn.commit()
 
     return DBJob(
-        type=type_, status=JobStatus.PENDING, source_type=source_type, **result
+        type=type_,
+        status=jobs.JobStatus.PENDING,
+        source_type=source_type,
+        **result,
     )
 
 
@@ -136,3 +127,66 @@ async def get_job_result_by_job_id(
         result = await cursor.fetchone()
 
     return DBOutput(**result) if result else None
+
+
+async def get_next_job(conn: AsyncConnection) -> DBJobFull | None:
+    # Acquires a reserved lock, blocking other write transactions
+    await conn.execute("BEGIN IMMEDIATE")
+
+    async with conn.execute(
+        f"""
+        SELECT id, type, status, source_type, source, created_at, updated_at
+        FROM jobs
+        WHERE status = '{jobs.JobStatus.PENDING.value}'
+        ORDER BY created_at ASC
+        LIMIT 1
+        """
+    ) as cursor:
+        result = await cursor.fetchone()
+
+    if not result:
+        # Releases the lock
+        await conn.rollback()
+        return None
+
+    db_job = DBJobFull(**result)
+
+    await conn.execute(
+        """
+        UPDATE jobs
+        SET status = 'in_progress', updated_at = DATETIME('now')
+        WHERE id = ?
+        """,
+        (db_job.id,),
+    )
+    await conn.commit()
+    db_job.status = jobs.JobStatus.IN_PROGRESS
+
+    return db_job
+
+
+async def finish_job(
+    conn: AsyncConnection,
+    job_id: int,
+    status: Literal[jobs.JobStatus.DONE, jobs.JobStatus.FAILED],
+    output: bytes | None,
+) -> None:
+    await conn.execute(
+        """
+        UPDATE jobs
+        SET status = ?, updated_at = DATETIME('now')
+        WHERE id = ?
+        """,
+        (status, job_id),
+    )
+
+    if output:
+        await conn.execute(
+            """
+            INSERT INTO outputs (job_id, output)
+            VALUES (?, ?)
+            """,
+            (job_id, output),
+        )
+
+    await conn.commit()
